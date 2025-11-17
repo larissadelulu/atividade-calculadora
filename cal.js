118371781897dc28db231c7d972d8c5ac955c773
LARISSA BARBIERI MACHADO <larissa_machado152@estudante.sesisenai.org.br>
2025-11-17 14:38:46 -0300

    Pasta deletada

---
+++ cal.js
@@ -0,0 +1,20 @@
+
+
+function soma (a,b) {
+    return a + b;
+}
+
+function subtracao (a,b){
+    return a - b;
+}
+
+function multiplicacao (a,b){
+    return a * b
+}
+
+function divisao (a,b){
+    if (b == 0){
+        return "Divisão zero"
+    }
+    return a / b
+}
